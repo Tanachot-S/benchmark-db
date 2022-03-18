97d1cce1e70c9c0f3717f310bb7b267d93b6f12f
Ball <Ball@ButthunMacM1.local>
2022-03-18 19:25:50 +0700

    edit connection & strategy

--- mongodb.ts
+++ mongodb.ts
@@ -1,61 +1,95 @@
-import { MongoClient } from "mongodb"
+import { MongoClient, ObjectId } from "mongodb"
 
 const mongoTransaction = async () => {
   const mongoClient = new MongoClient("mongodb://localhost:27017/benchmark")
   await mongoClient.connect()
-  console.log(">>>>>>>>>S")
   const collectionName = "benchmark"
 
-  // // delete all
-  // await mongoClient.db().dropCollection(collectionName)
+  // delete all
+  try {
+    await mongoClient.db().dropCollection(collectionName)
+  } catch(err) {
+    console.warn(err)
+  }
+  
+  // create collection
+  await mongoClient.db().createCollection(collectionName)
 
-  // // create collection
-  // await mongoClient.db().createCollection(collectionName)
+  const collection = mongoClient.db().collection("benchmark")
 
-  // const collection = mongoClient.db().collection("benchmark")
-  // console.log(">>>>>>>>>>>>>>>")
-  // console.time("dbsave")
+  // feed
+  const feed = []
+  for (let i = 0; i < 10000; i++) {
+    feed.push({
+      _id: i + "",
+      email: i,
+      stock: 0,
+    })
+  }
+  await collection.insertMany(feed)
 
-  // const session = mongoClient.startSession()
-  // await session.withTransaction(async () => {
-  //   // get
-  //   const count = collection.countDocuments()
-  //   console.log(count)
+  let retry = 0;
 
-  //   // insert
-  //   const ins = await collection.insertOne({
-  //     email: "abc@gmail.com"
-  //   }, {session})
-  //   console.log("ins", ins.insertedId)
+  // insert
+  const ins = await collection.insertOne({
+    email: "abc@gmail.com"
+  })
+  console.log("ins", ins.insertedId)
 
-  //   // update
-  //   const upd = await collection.updateOne({
-  //     _id: ins.insertedId
-  //   }, {
-  //     $set: { email: "def@gmail.com" },
-  //   }, {
-  //     session,
-  //   })
-  //   console.log("upd", upd.upsertedCount)
+  console.time("dbsave")
 
-  //   // get 
-  //   const get = await collection.find({}, {session}).toArray()
-  //   console.log("get", get.map(r => console.log(r)))
-    
-  //   // delete
-  //   const del = await collection.deleteOne({
-  //     _id: ins.insertedId
-  //   }, {session})
-  //   console.log("del", del.deletedCount)
-  // }, {
-  //   // readPreference: {
-  //   //   preference: ReadPreferenceMode.primary,
-  //   // },
-  //   readConcern: { level: "majority" },
-  //   writeConcern: { w: "majority" },
-  // })
+  await Promise.all(
+    [1,2,3,4,5].map((p) => {
+      return new Promise<void>(async (rs, rj) => {
+        for(let i = 0 ;i < 5000; i++) {
+          const session = mongoClient.startSession()
+          await session.withTransaction(async () => {
+            let sid = Math.floor(Math.random() * 10) + ""
+            retry++;
+            // count
+            // const count = await collection.countDocuments()
+            // console.log(count)
+         
+            // find all
+            const get = await collection.find({
+              _id: sid
+            }, {session}).toArray()
+            // console.log("get", get.map(r => console.log(r)))
+        
+            // update
+            const upd = await collection.updateOne({
+              _id: sid
+            }, {
+              $inc: { stock: 1 },
+            }, {
+              session,
+            })
+            // console.log("upd", upd.modifiedCount)
+  
+            // get
+            const gg = await collection.find({
+              _id: sid 
+            }, {session}).toArray()
+            // console.log("gg", gg.map(r => console.log(r)))
+            
+            // // delete
+            // const del = await collection.deleteOne({
+            //   _id: ins.insertedId
+            // }, {session})
+            // console.log("del", del.deletedCount)
+          }, {
+            readConcern: { level: "majority" },
+            writeConcern: { w: "majority" },
+          })
+        }
+        rs()
+      })
+    })
+  )
 
-  // console.timeEnd("dbsave")
+  console.timeEnd("dbsave")
+  console.log("total retry", retry)
+  console.log("rows", await collection.find({}).limit(10).toArray())
   process.exit(0)
 }
 
--- postgres.ts
+++ postgres.ts
@@ -21,50 +21,74 @@ const postgresTransaction = async () => {
   // create table
   await pool.query(`CREATE TABLE ${tableName} (
     id serial PRIMARY KEY,
-	  email VARCHAR ( 255 ) UNIQUE NOT NULL
+	  email VARCHAR ( 255 ) UNIQUE NOT NULL,
+    stock INT NOT NULL
   );`)
-  // await fromPostgresClient.query(
-  //   `SET search_path TO '${config.from.prisma.project}$${config.from.prisma.stage}'`,
-  // )
 
-  const client = await pool.connect()
-  try {
-    console.time("dbsave")
-    await client.query("BEGIN")
-    // count
-    const countQ = `SELECT COUNT(*) FROM ${tableName}`
-    const c = await client.query(countQ)
-    console.log("get", c.rows[0])
-
-    // insert
-    const insQ = `INSERT INTO ${tableName}(email) VALUES($1) RETURNING id`
-    const res = await client.query(insQ, ['abc@gmail.com'])
-    console.log("ins", res.rows[0])
-
-    // update
-    const upQ = `UPDATE ${tableName} SET email = $1`
-    const u = await client.query(upQ, ['def@gmail.com'])
-    console.log("up", u.rows[0])
-
-    // get
-    const getQ = `SELECT * FROM ${tableName}`
-    const g = await client.query(getQ)
-    console.log("get", g.rows[0])
-
-    // delete
-    const delQ = `DELETE FROM ${tableName} WHERE 1=1`
-    const d = await client.query(delQ)
-    console.log("del",d.rows)
-   
-    await client.query("COMMIT")
-  } catch (e) {
-    await client.query("ROLLBACK")
-    throw e
-  } finally {
-    client.release()
-    console.timeEnd("dbsave")
+  // feed
+  for(let i = 0; i < 10000; i++) {
+    const insQ = `INSERT INTO ${tableName}(email, stock) VALUES($1, $2) RETURNING id`
+    await pool.query(insQ, [i + "", 0]) 
   }
 
+  // insert
+  const insQ = `INSERT INTO ${tableName}(email, stock) VALUES($1, $2) RETURNING id`
+  const res = await pool.query(insQ, ['abc@gmail.com', 0])
+  console.log("ins", res.rows[0])
+
+  let retry = 0;
+
+  console.time("dbsave")
+  await Promise.all(
+    [1,2,3,4,5].map((p) => {
+      return new Promise<void>(async (rs, rj) => {
+        const client = await pool.connect()
+        for(let i = 0 ;i < 5000; i++) {
+          try {
+            let sid = 1 + Math.floor(Math.random() * 10)
+            await client.query("BEGIN")
+            retry++;
+            // count
+            // const countQ = `SELECT COUNT(*) FROM ${tableName}`
+            // const c = await client.query(countQ)
+            // console.log("get", c.rows[0])
+        
+            // find all
+            const getQ = `SELECT * FROM ${tableName} WHERE id = $1 FOR UPDATE`
+            const g = await client.query(getQ, [sid])
+            // console.log("get lock...", g.rows[0])
+        
+            // update
+            const upQ = `UPDATE ${tableName} SET stock = $1 WHERE id = $2`
+            const u = await client.query(upQ, [g.rows[0].stock + 1, sid])
+            // console.log("up", u.rows[0])
+
+            // get
+            const getQ2 = `SELECT * FROM ${tableName} WHERE id = $1`
+            const g2 = await client.query(getQ2, [sid])
+            // console.log("gg", g2.rows[0])
+            
+            // // delete
+            // const delQ = `DELETE FROM ${tableName} WHERE 1=1`
+            // const d = await client.query(delQ)
+            // console.log("del", d.rows)
+          
+            await client.query("COMMIT")
+           
+          } catch (e) {
+            console.log("err", e)
+            await client.query("ROLLBACK")
+            // rj()
+          } 
+        }
+        rs()
+        client.release()
+      })
+    })
+  )
+
+  console.timeEnd("dbsave")
+  console.log("total retry", retry)
   process.exit(0)
 }
 
